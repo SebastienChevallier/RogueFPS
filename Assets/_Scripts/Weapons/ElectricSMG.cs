b59185ddffd5dec718968f0af1392e9e6c9b90b1
Sebastien chevallier <s.chevallier.pro@gmail.com>
2025-05-27 10:39:34 +0200

    FlipperModels

--- Assets/_Scripts/Weapons/ElectricSMG.cs
+++ Assets/_Scripts/Weapons/ElectricSMG.cs
@@ -5,6 +5,7 @@ public class ElectricSMG : BaseWeapon
 {
     public float distance;
     public LayerMask layerMask;
+    public int Damage;
     public override void Shoot()
     {
         if (!isRecoiling)
@@ -27,7 +28,7 @@ public class ElectricSMG : BaseWeapon
 
             if (hit.collider.TryGetComponent<I_Health>(out I_Health health))
             {
-                health.
+                health.OnDecreaseHealth(Damage);
             }
         }
         return hits;
