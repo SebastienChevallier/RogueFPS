9b2fd05f8778a5005dd733fb07e0ea17e9ada240
OMAILLOT <orianmaillot@gmail.com>
2025-06-17 15:33:36 +0200

    push

--- Assets/_Scripts/Entity/E_RagdollEffect.cs
+++ Assets/_Scripts/Entity/E_RagdollEffect.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -6,6 +7,8 @@ public class E_RagdollEffect : MonoBehaviour
     [Header("references")]
     public Rigidbody body;
     public NavMeshAgent agent;
+    public Collider mainCollider;
+    public Rigidbody spineBody;
 
     Rigidbody[] rigidbodies;
     Collider[] ragdollColliders;
@@ -14,10 +17,24 @@ public class E_RagdollEffect : MonoBehaviour
     public float maxRagdollToDisable;
     public bool isEnableRagdoll;
 
+    public List<BoneInfo> boneInfos = new List<BoneInfo>();
+
     private void Start()
     {
         rigidbodies = GetComponentsInChildren<Rigidbody>();
         ragdollColliders = GetComponentsInChildren<Collider>();
+
+        var ragdollBones = GetComponentsInChildren<Rigidbody>();
+        foreach (var rb in ragdollBones)
+        {
+            BoneInfo info = new BoneInfo();
+            info.bone = rb.transform;
+            info.position = rb.transform.localPosition;
+            info.rotation = rb.transform.localRotation;
+            boneInfos.Add(info);
+        }
+
+
         ToggleRagdoll(true);
     }
 
@@ -32,7 +49,7 @@ public class E_RagdollEffect : MonoBehaviour
             }
         } else
         {
-            if (body.linearVelocity.magnitude <= maxRagdollToDisable)
+            if (spineBody.linearVelocity.magnitude <= maxRagdollToDisable)
             {
                 DisableRagdoll();
             }
@@ -45,6 +62,7 @@ public class E_RagdollEffect : MonoBehaviour
         body.constraints = RigidbodyConstraints.None;
         agent.enabled = false;
         ToggleRagdoll(false);
+        spineBody.linearVelocity = body.linearVelocity;
     }
 
     public void DisableRagdoll()
@@ -54,6 +72,12 @@ public class E_RagdollEffect : MonoBehaviour
         body.constraints = RigidbodyConstraints.FreezeRotation;
         agent.enabled = true;
         ToggleRagdoll(true);
+
+        foreach (var info in boneInfos)
+        {
+            info.bone.localPosition = info.position;
+            info.bone.localRotation = info.rotation;
+        }
     }
 
     void ToggleRagdoll(bool activate)
@@ -66,5 +90,15 @@ public class E_RagdollEffect : MonoBehaviour
         {
             col.enabled = !activate;
         }
+        
+        mainCollider.enabled = activate;
     }
+}
+
+
+public class BoneInfo
+{
+    public Transform bone;
+    public Vector3 position;
+    public Quaternion rotation;
 }
